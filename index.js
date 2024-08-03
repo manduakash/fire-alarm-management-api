969325cc2055abb006d1dec24e696b807eff1519
Akash Singh <manduakash@gmail.com>
2024-08-03 16:20:14 +0530

    almighty push

--- index.js
+++ index.js
@@ -12,10 +12,8 @@ const apiRoutes = require('./src/routes/api');
 const panelRoutes = require('./src/routes/panelRoutes');
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/akash-db', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
+mongoose.connect(process.env.MONGODB_URI, {
+  }).then(() => {
   console.log('Connected to MongoDB');
 }).catch((err) => {
   console.error('Error connecting to MongoDB:', err);
